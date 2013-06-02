280cd0b327b73c63f0d9178ef0600faf834d265e
James McKinney <james@slashpoundbang.com>
2013-06-02 19:23:52 -0400

    add TfIdfModel

---
+++ lib/tf-idf-similarity/tf_idf_model.rb
@@ -0,0 +1,81 @@
+# A document-term matrix using either the tf*idf or BM25 functions.
+#
+# @see http://lucene.apache.org/core/4_0_0-BETA/core/org/apache/lucene/search/similarities/TFIDFSimilarity.html
+# @see http://lucene.apache.org/core/4_0_0-BETA/core/org/apache/lucene/search/similarities/BM25Similarity.html
+# @see http://en.wikipedia.org/wiki/Okapi_BM25
+class TfIdfSimilarity::TfIdfModel
+  include TfIdfSimilarity::MatrixMethods
+
+  extend Forwardable
+  def_delegators :@model, :documents, :terms
+
+  # @param [Array<TfIdfSimilarity::Document>] documents documents
+  # @param [Hash] opts optional arguments
+  # @option opts [Symbol] :library :gsl, :narray, :nmatrix or :matrix (default)
+  # @option opts [Symbol] :function :tfidf (default) or :bm25
+  def initialize(documents, opts = {})
+    @model = TfIdfSimilarity::TermCountModel.new(documents, opts)
+    @library = (opts[:library] || :matrix).to_sym
+    @function = (opts[:function] || :tfidf).to_sym
+
+    array = Array.new(terms.size) do |i|
+      idf = inverse_document_frequency(terms[i])
+      Array.new(documents.size) do |j|
+        term_frequency(documents[j], terms[i]) * idf
+      end
+    end
+
+    @matrix = initialize_matrix(array)
+  end
+
+  # Return the term's inverse document frequency.
+  #
+  # @param [String] term a term
+  # @return [Float] the term's inverse document frequency
+  def inverse_document_frequency(term)
+    df = @model.document_count(term)
+    if @function == :bm25
+      log((documents.size - df + 0.5) / (df + 0.5))
+    else
+      1 + log(documents.size / (df + 1.0))
+    end
+  end
+  alias_method :idf, :inverse_document_frequency
+
+  # Returns the term's frequency in the document.
+  #
+  # @param [Document] document a document
+  # @param [String] term a term
+  # @return [Float] the term's frequency in the document
+  #
+  # @note Like Lucene, we use a b value of 0.75 and a k1 value of 1.2.
+  def term_frequency(document, term)
+    tf = document.term_count(term)
+    if @function == :bm25
+      (tf * 2.2) / (tf + 0.3 + 0.9 * documents.size / @model.average_document_size)
+    else
+      sqrt(tf)
+    end
+  end
+  alias_method :tf, :term_frequency
+
+  # Return the term frequency–inverse document frequency.
+  #
+  # @param [Document] document a document
+  # @param [String] term a term
+  # @return [Float] the term frequency–inverse document frequency
+  def term_frequency_inverse_document_frequency(document, term)
+    inverse_document_frequency(term) * term_frequency(document, term)
+  end
+  alias_method :tfidf, :term_frequency_inverse_document_frequency
+
+  # Returns a similarity matrix for the documents in the corpus.
+  #
+  # @return [GSL::Matrix,NMatrix,Matrix] a similarity matrix
+  # @note Columns are normalized to unit vectors, so we can calculate the cosine
+  #   similarity of all document vectors. BM25 doesn't normalize columns, but
+  #   BM25 wasn't written with this use case in mind.
+  def similarity_matrix
+    multiply_self(normalize)
+  end
+end
---
+++ spec/tf_idf_model_spec.rb
@@ -0,0 +1,174 @@
+require 'spec_helper'
+
+describe TfIdfSimilarity::TfIdfModel do
+  let :text do
+    "FOO-foo BAR bar \r\n\t 123 !@#"
+  end
+
+  let :tokens do
+    ['FOO-foo', 'BAR', 'bar', "\r\n\t", '123', '!@#']
+  end
+
+  let :document_without_text do
+    TfIdfSimilarity::Document.new('')
+  end
+
+  let :document do
+    TfIdfSimilarity::Document.new(text)
+  end
+
+  let :document_with_tokens do
+    TfIdfSimilarity::Document.new(text, :tokens => tokens)
+  end
+
+  let :document_with_term_counts do
+    TfIdfSimilarity::Document.new(text, :term_counts => {'bar' => 5, 'baz' => 10})
+  end
+
+  let :non_corpus_document do
+    TfIdfSimilarity::Document.new('foo foo foo')
+  end
+
+  def similarity_matrix_values(model)
+    matrix = model.similarity_matrix
+    if MATRIX_LIBRARY == :nmatrix
+      matrix.each.to_a
+    else
+      matrix.to_a.flatten
+    end
+  end
+
+  context 'without documents' do
+    let :model do
+      TfIdfSimilarity::TfIdfModel.new([], :library => MATRIX_LIBRARY)
+    end
+
+    describe '#documents' do
+      it 'should be empty' do
+        model.documents.should be_empty
+      end
+    end
+
+    describe '#terms' do
+      it 'should be empty' do
+        model.terms.should be_empty
+      end
+    end
+
+    describe '#inverse_document_frequency' do
+      it 'should return negative infinity' do
+        model.idf('foo').should == -1/0.0 # -Infinity
+      end
+    end
+
+    describe '#term_frequency' do
+      it 'should return the term frequency' do
+        model.tf(document, 'foo').should == Math.sqrt(2)
+      end
+    end
+
+    describe '#term_frequency_inverse_document_frequency' do
+      it 'should return negative infinity' do
+        model.tfidf(document, 'foo').should == -1/0.0 # -Infinity
+      end
+    end
+
+    describe '#similarity_matrix' do
+      it 'should be empty' do
+        similarity_matrix_values(model).should be_empty
+      end
+    end
+  end
+
+  context 'with documents' do
+    let :documents do
+      [
+        document,
+        document_with_tokens,
+        document_without_text,
+        document_with_term_counts,
+      ]
+    end
+
+    let :model do
+      TfIdfSimilarity::TfIdfModel.new(documents, :library => MATRIX_LIBRARY)
+    end
+
+    describe '#documents' do
+      it 'should return the documents' do
+        model.documents.should == documents
+      end
+    end
+
+    describe '#terms' do
+      it 'should return the terms' do
+        model.terms.to_a.sort.should == ['bar', 'baz', 'foo', 'foo-foo']
+      end
+    end
+
+    describe '#inverse_document_frequency' do
+      it 'should return the inverse document frequency' do
+        model.idf('foo').should be_within(0.001).of(1 + Math.log(2))
+      end
+
+      it 'should return the inverse document frequency of a non-occurring term' do
+        model.idf('xxx').should be_within(0.001).of(1 + Math.log(4))
+      end
+    end
+
+    describe '#term_frequency' do
+      it 'should return the term frequency if no tokens given' do
+        model.tf(document, 'foo').should == Math.sqrt(2)
+      end
+
+      it 'should return the term frequency if tokens given' do
+        model.tf(document_with_tokens, 'foo-foo').should == 1
+      end
+
+      it 'should return no term frequency if no text given' do
+        model.tf(document_without_text, 'foo').should == 0
+      end
+
+      it 'should return the term frequency if term counts given' do
+        model.tf(document_with_term_counts, 'bar').should == Math.sqrt(5)
+      end
+
+      it 'should return the term frequency of a non-occurring term' do
+        model.tf(document, 'xxx').should == 0
+      end
+
+      it 'should return the term frequency in a non-occurring document' do
+        model.tf(non_corpus_document, 'foo').should == Math.sqrt(3)
+      end
+    end
+
+    describe '#term_frequency_inverse_document_frequency' do
+      it 'should return the tf*idf' do
+        model.tfidf(document, 'foo').should be_within(0.001).of((1 + Math.log(2)) * Math.sqrt(2))
+      end
+
+      it 'should return the tf*idf of a non-occurring term' do
+        model.tfidf(document, 'xxx').should == 0
+      end
+
+      it 'should return the tf*idf in a non-occurring term' do
+        model.tfidf(non_corpus_document, 'foo').should be_within(0.001).of((1 + Math.log(2)) * Math.sqrt(3))
+      end
+    end
+
+    describe '#similarity_matrix' do
+      it 'should return the similarity matrix' do
+        expected = [
+          1.0,   0.326, 0.0, 0.195,
+          0.326, 1.0,   0.0, 0.247,
+          0.0,   0.0,   0.0, 0.0,
+          0.195, 0.247, 0.0, 1.0,
+        ]
+
+        similarity_matrix_values(model).each_with_index do |value,i|
+          value.should be_within(0.001).of(expected[i])
+        end
+      end
+    end
+  end
+end
