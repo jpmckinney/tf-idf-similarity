046dbf8cc97ce21ddb285fa56b7c2f7ee4e19782
James McKinney <james@slashpoundbang.com>
2015-01-12 15:26:44 -0500

    skip spec in coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,11 @@
 require 'rubygems'
 
+require 'simplecov'
 require 'coveralls'
-Coveralls.wear!
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+  add_filter 'spec'
+end
 
 require 'rspec'
 require File.dirname(__FILE__) + '/../lib/tf-idf-similarity'
